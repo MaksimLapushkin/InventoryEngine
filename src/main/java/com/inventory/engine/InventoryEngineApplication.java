19ad838765c6200ad109cc2948955571f2eff19e
Maksim Lapushkin <maxim.lapushkin.98@gmail.com>
2026-04-13 16:31:47 +0200

    fix: mistype in code fixed

--- src/main/java/com/inventory/engine/InventoryEngineApplication.java
+++ src/main/java/com/inventory/engine/InventoryEngineApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class giInventoryEngineApplication {
+public class InventoryEngineApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryEngineApplication.class, args);
     }
