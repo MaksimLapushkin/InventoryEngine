5527c6749eab1b9006cb214f7bc48f3819bce015
Maksim Lapushkin <maxim.lapushkin.98@gmail.com>
2026-04-13 16:03:29 +0200

    update: gitignore

--- src/main/java/com/inventory/engine/InventoryEngineApplication.java
+++ src/main/java/com/inventory/engine/InventoryEngineApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class InventoryEngineApplication {
+public class giInventoryEngineApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryEngineApplication.class, args);
     }
